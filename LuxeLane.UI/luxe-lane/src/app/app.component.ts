7634f3d64f7d6395af0b39beedafc8b574976cad
Souvik Kundu <souvikvip@outlook.com>
2024-04-07 02:14:38 +0530

    added nav ui

--- LuxeLane.UI/luxe-lane/src/app/app.component.ts
+++ LuxeLane.UI/luxe-lane/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavComponent]
 })
 export class AppComponent {
   title = 'luxe-lane';
