9b7df7b9c12558f99d16faea7bdbf05b5df92bd4
Souvik Kundu <souvikvip@outlook.com>
2024-04-13 23:59:59 +0530

    added admin module for admin based operations

---
+++ LuxeLane.UI/luxe-lane/src/app/admin/adminnav/adminnav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminnav',
+  standalone: true,
+  imports: [],
+  templateUrl: './adminnav.component.html',
+  styleUrl: './adminnav.component.css'
+})
+export class AdminnavComponent {
+
+}
--- LuxeLane.UI/luxe-lane/src/app/app.routes.ts
+++ LuxeLane.UI/luxe-lane/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+    imports : [RouterModule.forRoot(routes)],
+    exports : [RouterModule]
+})
+
+export class AppRoutingModule {}
+
